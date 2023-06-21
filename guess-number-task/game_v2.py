f0ba2615a9dfd3f3fd9b809cd0afa13f77c02ad7
Deemetr5 <dmalsokoloff@gmail.com>
2023-06-21 20:38:40 +0400

    add seed

--- guess-number-task/game_v2.py
+++ guess-number-task/game_v2.py
@@ -25,7 +25,7 @@ def random_predict(number: int = 1) -> int:
 
 
 def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -34,7 +34,7 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(14)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
