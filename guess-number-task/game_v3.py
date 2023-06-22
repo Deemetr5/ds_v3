aa0d64ffa4c5cbb76d1b3c9b4363714f555a8402
Deemetr5 <dmalsokoloff@gmail.com>
2023-06-22 16:52:33 +0400

    edit v3 file

--- guess-number-task/game_v3.py
+++ guess-number-task/game_v3.py
@@ -18,7 +18,11 @@ def random_predict(number: int = 1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        predict_number = np.random.randint(1, 50)  # предполагаемое число
+        if number < predict_number < 50:
+            predict_number = np.random.randint(1, 49)  # предполагаемое число
+            if number == predict_number:
+                break  # выход из цикла если угадали
         if number == predict_number:
             break  # выход из цикла если угадали
     return count
